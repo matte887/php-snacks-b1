237431e309a743394ea099d66e0e38077827c2c6
matte887 <m.erroi92@gmail.com>
2022-06-08 16:14:35 +0200

    Snack 3

---
+++ Snack-3/index.php
@@ -0,0 +1,76 @@
+<!-- ## Snack 3
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post. -->
+
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 3</title>
+</head>
+<body>
+
+<?php
+
+$dates = array_keys($posts);
+
+for ($i=0; $i < count($dates); $i++) { 
+    $this_day = $dates[$i];
+    echo "<p>$this_day</p>";
+
+    for ($j=0; $j < count($posts[$this_day]); $j++) {
+        $this_day_posts = $posts[$this_day]; 
+    ?>
+        <h3><?php echo $this_day_posts[$j]['title'] ?></h3>
+        <h4><?php echo $this_day_posts[$j]['author'] ?></h4>
+        <p><?php echo $this_day_posts[$j]['text'] ?></p>
+    <?php } ?>
+<?php } ?>
+
+</body>
+</html>
