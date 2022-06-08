8f0d7cf688d563d87dd85cb46c3a38cba81d30d1
matte887 <m.erroi92@gmail.com>
2022-06-08 14:12:58 +0200

    Snack 1

--- Snack-1/index.php
+++ Snack-1/index.php
@@ -1,3 +1,64 @@
 <!-- ## Snack 1
 Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60 -->
+
+<?php
+$match = [
+    [
+        "squadra_casa" => "Milano",
+        "squadra_ospite" => "Cantù",
+        "punti_casa" => 21,
+        "punti_ospite" => 34,
+    ],
+    [
+        "squadra_casa" => "Bologna",
+        "squadra_ospite" => "Firenze",
+        "punti_casa" => 11,
+        "punti_ospite" => 2,
+    ],
+    [
+        "squadra_casa" => "Teramo",
+        "squadra_ospite" => "Arezzo",
+        "punti_casa" => 24,
+        "punti_ospite" => 25,
+    ],
+    [
+        "squadra_casa" => "Prato",
+        "squadra_ospite" => "Trieste",
+        "punti_casa" => 65,
+        "punti_ospite" => 86,
+    ],
+    [
+        "squadra_casa" => "Palermo",
+        "squadra_ospite" => "Pesaro",
+        "punti_casa" => 84,
+        "punti_ospite" => 35,
+    ],
+];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 1</title>
+</head>
+
+<body>
+    <?php
+    for ($i = 0; $i < count($match); $i++) {
+    ?>
+        <h3>Partita <?php echo $i + 1 ?></h3>
+        <p>
+            <?php
+            echo $match[$i]["squadra_casa"] . " - " . $match[$i]["squadra_ospite"] .
+            " | ".
+            $match[$i]["punti_casa"] . "-" . $match[$i]["punti_ospite"] ?>
+        </p>
+    <?php } ?>
+</body>
+
+</html>
